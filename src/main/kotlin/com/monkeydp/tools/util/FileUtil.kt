04c9e174ecbcfd13c0fb47098f638eae9e83295d
xuchen <monkeydpotato@gmail.com>
2019-10-17 18:48:49 +0800

    list files by FileFilter

--- src/main/kotlin/com/monkeydp/tools/util/FileUtil.kt
+++ src/main/kotlin/com/monkeydp/tools/util/FileUtil.kt
@@ -2,6 +2,7 @@ package com.monkeydp.tools.util
 
 import com.monkeydp.tools.exception.inner.StdInnerException
 import java.io.File
+import java.io.FileFilter
 import java.io.FilenameFilter
 
 /**
@@ -9,8 +10,9 @@ import java.io.FilenameFilter
  * @date 2019/10/13
  */
 object FileUtil {
+
     /**
-     * List all files under the given directory
+     * List all files under the given directory path by FilenameFilter
      */
     fun listFiles(dirPath: String, filter: FilenameFilter): Array<File> {
         val dir = File(dirPath)
@@ -18,9 +20,26 @@ object FileUtil {
     }
 
     /**
-     * List all files under the given directory
+     * List all files under the given directory by FilenameFilter
      */
     fun listFiles(dir: File, filter: FilenameFilter): Array<File> {
+        checkIsDir(dir)
+        val files: Array<File>? = dir.listFiles(filter)
+        return files!!
+    }
+
+    /**
+     * List all files under the given directory path by FileFilter
+     */
+    fun listFiles(dirPath: String, filter: FileFilter): Array<File> {
+        val dir = File(dirPath)
+        return listFiles(dir, filter)
+    }
+
+    /**
+     * List all files under the given directory by FileFilter
+     */
+    fun listFiles(dir: File, filter: FileFilter): Array<File> {
         checkIsDir(dir)
         val files: Array<File>? = dir.listFiles(filter)
         return files!!
