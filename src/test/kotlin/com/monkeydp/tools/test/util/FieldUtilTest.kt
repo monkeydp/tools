8ce18f37a9e734ce42dc5dba3966ffb3652fc184
iPotato <monkeydpotato@gmail.com>
2019-10-30 01:00:03 +0800

    *

--- src/test/kotlin/com/monkeydp/tools/test/util/FieldUtilTest.kt
+++ src/test/kotlin/com/monkeydp/tools/test/util/FieldUtilTest.kt
@@ -11,47 +11,44 @@ import org.junit.Test
 class FieldUtilTest {
     private companion object {
         const val NOT_EXIST_FIELD_NAME = "notExist"
-
-        const val PARENT_NAME = "dad"
-        const val CHILD_NAME = "son"
     }
-
+    
     open class Parent {
-        open var name = PARENT_NAME
+        open var name = "dad"
         val phone = "15845678910"
         val age = 22
     }
-
+    
     class Child : Parent() {
-        override var name = CHILD_NAME
+        override var name = "son"
     }
-
+    
     private fun mockChild(): Child {
         return Child()
     }
-
+    
     @Test
     fun getFieldTest() {
         getFieldSuccessTest()
         getFieldFailTest()
     }
-
+    
     private fun getFieldSuccessTest() {
-        val field = FieldUtil.getField(mockChild(), "phone")
+        val field = FieldUtil.getField(mockChild(), Parent::phone.name)
         Assert.assertNotNull(field)
     }
-
+    
     private fun getFieldFailTest() {
         val field = FieldUtil.getField(mockChild(), NOT_EXIST_FIELD_NAME, true)
         Assert.assertNull(field)
     }
-
+    
     @Test
     fun getDeclaredFieldsTest() {
         val declaredFields = FieldUtil.getDeclaredFields(Child::class.java)
         Assert.assertTrue(declaredFields.size == 1)
     }
-
+    
     @Test
     fun getFieldsTest() {
         val fields = FieldUtil.getFields(Child::class.java)
