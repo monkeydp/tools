b4ccd35bd1ad2d66ff47365d0dbaf97b621dc226
iPotato <monkeydpotato@gmail.com>
2019-10-30 16:37:05 +0800

    add List ext

--- src/main/kotlin/com/monkeydp/tools/ext/List.kt
+++ src/main/kotlin/com/monkeydp/tools/ext/List.kt
@@ -2,21 +2,6 @@ package com.monkeydp.tools.ext
 
 /**
  * @author iPotato
- * @date 2019/10/24
+ * @date 2019/10/30
  */
-fun <E> MutableList<E>.append(vararg list: E) {
-    this.addAll(list)
-}
-
-fun <E> MutableList<E>.replace(old: E, new: E) {
-    val index = indexOf(old)
-    this[index] = new
-}
-
-fun <E> MutableList<E>.replaceLast(new: E) {
-    this[this.lastIndex] = new
-}
-
-fun <E> MutableList<E>.removeFirst() {
-    this.removeAt(0)
-}
+fun <E> List<E>.lastOf(index: Int) = get(size - index - 1)
---
+++ src/main/kotlin/com/monkeydp/tools/ext/MutableList.kt
@@ -0,0 +1,23 @@
+package com.monkeydp.tools.ext
+
+/**
+ * @author iPotato
+ * @date 2019/10/24
+ */
+fun <E> MutableList<E>.append(vararg list: E) {
+    this.addAll(list)
+}
+
+fun <E> MutableList<E>.replace(old: E, new: E) {
+    val index = indexOf(old)
+    this[index] = new
+}
+
+fun <E> MutableList<E>.replaceLast(new: E) {
+    this[this.lastIndex] = new
+}
+
+fun <E> MutableList<E>.removeFirst() {
+    this.removeAt(0)
+}
+
