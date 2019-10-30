899e4c7b31e36f460383129d8c328c41f7cf3501
iPotato <monkeydpotato@gmail.com>
2019-10-30 14:42:51 +0800

    Any opt

--- src/main/kotlin/com/monkeydp/tools/function/Any.kt
+++ src/main/kotlin/com/monkeydp/tools/function/Any.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.monkeydp.tools.function
 
 import java.util.*
@@ -8,20 +10,30 @@ import kotlin.reflect.full.memberProperties
  * @author iPotato
  * @date 2019/10/29
  */
-inline fun <reified T : Any> T.toProps(): Properties {
+fun Any.toProps(): Properties {
     val properties = Properties()
-    T::class.memberProperties.forEach { properties[it.name] = it.get(this) }
+    this.javaClass.kotlin.memberProperties.forEach { properties[it.name] = it.get(this) }
     return properties
 }
 
-inline fun <reified T : Any> T.toDeclaredProps(any: T): Properties {
+fun Any.toDeclaredProps(): Properties {
     val properties = Properties()
-    T::class.declaredMemberProperties.forEach { properties[it.name] = it.get(any) }
+    this.javaClass.kotlin.declaredMemberProperties.forEach { properties[it.name] = it.get(this) }
     return properties
 }
 
-inline fun <reified T : Any> T.toPropMap() =
-        T::class.memberProperties.map { it.name to it.get(this) }.toMap()
+fun Any.toPropMap() = this.javaClass.kotlin.memberProperties.map { it.name to it.get(this) }.toMap()
 
-inline fun <reified T : Any> T.toDeclaredPropMap() =
-        T::class.declaredMemberProperties.map { it.name to it.get(this) }.toMap()
+fun <K, V> Any.toPropMapX() = toPropMap() as Map<K, V>
+
+fun Any.toDeclaredPropMap() = this.javaClass.kotlin.declaredMemberProperties.map { it.name to it.get(this) }.toMap()
+
+fun <K, V> Any.toDeclaredPropMapX() = toDeclaredPropMap() as Map<K, V>
+
+fun Any.toPropList() = this.javaClass.kotlin.memberProperties.map { it.get(this) }.toList()
+
+fun <T> Any.toPropListX() = toPropList() as List<T>
+
+fun Any.toDeclaredPropList() = this.javaClass.kotlin.declaredMemberProperties.map { it.get(this) }.toList()
+
+fun <T> Any.toDeclaredPropListX() = toDeclaredPropList() as List<T>
