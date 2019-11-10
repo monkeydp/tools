fcb2f347583269c60ca8db03dd1785bcb015c28b
iPotato <monkeydpotato@gmail.com>
2019-11-10 15:42:27 +0800

    refactor SdkImplRegistry

---
+++ src/main/kotlin/com/monkeydp/tools/ext/KProperty.kt
@@ -0,0 +1,22 @@
+package com.monkeydp.tools.ext
+
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl
+import sun.reflect.generics.reflectiveObjects.WildcardTypeImpl
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+import kotlin.reflect.jvm.javaType
+
+/**
+ * @author iPotato
+ * @date 2019/11/9
+ */
+// TODO Need test
+fun KProperty<*>.getFirstUpperBound(): KClass<*> {
+    val returnJavaType = returnType.javaType as ParameterizedTypeImpl
+    val wildcardType = returnJavaType.actualTypeArguments[0] as WildcardTypeImpl
+    return when (val type = wildcardType.upperBounds[0]) {
+        is ParameterizedTypeImpl -> type.rawType!!.kotlin
+        is Class<*> -> type.kotlin
+        else -> ierror("Unknown type $type!")
+    }
+}
