d8d3aa71af30491c7357dae18768546067b9b3cf
iPotato <monkeydpotato@gmail.com>
2019-12-05 22:12:16 +0800

    apply aspectj to all project

---
+++ src/main/kotlin/com/monkeydp/tools/config/DevModeConfig.kt
@@ -0,0 +1,24 @@
+package com.monkeydp.tools.config
+
+import com.monkeydp.tools.config.DevMode.DEBUG
+import com.monkeydp.tools.config.DevMode.NORMAL
+import com.monkeydp.tools.ext.lines
+import org.slf4j.Logger
+
+/**
+ * @author iPotato
+ * @date 2019/12/2
+ */
+enum class DevMode {
+    DEBUG,
+    NORMAL;
+}
+
+var devModel = NORMAL
+
+fun enableDebugMode() {
+    devModel = DEBUG
+}
+
+fun Logger.debugMode(e: Throwable): Unit =
+        debug("<DEBUG MODE> ${e.message}${System.lineSeparator()}${e.stackTrace.toList().lines()}}")
