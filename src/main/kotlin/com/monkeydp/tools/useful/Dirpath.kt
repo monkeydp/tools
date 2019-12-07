c3c562ccae5cf2be6308b32c1edad6cedefb2643
iPotato <monkeydpotato@gmail.com>
2019-12-07 15:11:29 +0800

    *

---
+++ src/main/kotlin/com/monkeydp/tools/useful/Dirpath.kt
@@ -0,0 +1,17 @@
+package com.monkeydp.tools.useful
+
+import com.monkeydp.tools.ext.toStdPath
+
+/**
+ * @author iPotato
+ * @date 2019/12/7
+ */
+interface Dirpath {
+    val root: String get() = System.getProperty("user.dir").toStdPath()
+    val src get() = "$root/src"
+    val main get() = "$src/main"
+    val build get() = "$root/build"
+    val classes get() = "$build/classes"
+    val generated get() = "$src/generated"
+    val kotlinGenerated get() = "$generated/kotlin"
+}
