8a53ca67cb53737ab01be02924702b161913d16f
iPotato-Work <monkeydpotato@gmail.com>
2020-05-18 17:38:30 +0800

    *

--- src/main/kotlin/com/monkeydp/tools/ext/kotlin/Annotation.kt
+++ src/main/kotlin/com/monkeydp/tools/ext/kotlin/Annotation.kt
@@ -8,13 +8,16 @@ import kotlin.reflect.KProperty1
  * @author iPotato-Work
  * @date 2020/5/18
  */
+
 /**
  * Change annotation attributes
  */
-fun <T : Annotation, R : Any> T.changeAttrs(pair: Pair<KProperty1<T, R>, R>) {
+fun <T : Annotation, R : Any> T.changeAttrs(vararg pairs: Pair<KProperty1<T, R>, R>) {
     if (this !is Proxy) throw RuntimeException("Require proxy of annotation!")
-    val (kProperty1, propertyValue) = pair
     val h: InvocationHandler = this.getFieldValue("h") { forceAccess = true }
     val memberValues: MutableMap<String, Any> = h.getFieldValue("memberValues") { forceAccess = true }
-    memberValues[kProperty1.name] = propertyValue
+    pairs.forEach {
+        val (kProperty1, propertyValue) = it
+        memberValues[kProperty1.name] = propertyValue
+    }
 }
