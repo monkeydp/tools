8dd648e37ded42b548f27a384b7a831e14a6150d
iPotato-Work <monkeydpotato@gmail.com>
2020-05-29 13:38:06 +0800

    *

--- src/main/kotlin/com/monkeydp/tools/ext/kotlin/Number.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.tools.ext.kotlin
-
-import com.monkeydp.tools.util.RandomUtil
-
-/**
- * @author iPotato-Work
- * @date 2020/5/7
- */
-fun Int.random(min: Int = 1) = RandomUtil.randomInt(min, this)
