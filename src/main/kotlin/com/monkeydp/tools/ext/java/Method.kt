8e1d392e4387c35680a243cb4323cdb0c8f10ccb
iPotato-Work <monkeydpotato@gmail.com>
2020-06-12 13:34:07 +0800

    *

--- src/main/kotlin/com/monkeydp/tools/ext/java/Method.kt
+++ src/main/kotlin/com/monkeydp/tools/ext/java/Method.kt
@@ -13,8 +13,21 @@ val Method.inClass get() = FieldUtil.getValue<Class<*>>(this, "clazz") { forceAc
 val Method.inKClass get() = inClass.kotlin
 
 @Suppress("UNCHECKED_CAST")
-fun <T : Annotation> Method.findAnnot(annotKClass: KClass<out T>): T =
-        annotations.single() { it.annotationClass == annotKClass } as T
+fun <T : Annotation> Method.findAnnot(annotKClass: KClass<out T>) =
+        annotations.single { it.annotationClass == annotKClass } as T
 
-inline fun <reified T : Annotation> Method.findAnnot(): T =
-        annotations.single() { it is T } as T
+inline fun <reified T : Annotation> Method.findAnnot() =
+        annotations.single { it is T } as T
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Annotation> Method.findAnnotOrNull(annotKClass: KClass<out T>) =
+        annotations.singleOrNull { it.annotationClass == annotKClass } as T?
+
+inline fun <reified T : Annotation> Method.findAnnotOrNull() =
+        annotations.singleOrNull { it is T } as T?
+
+fun <T : Annotation> Method.hasAnnot(annotKClass: KClass<out T>) =
+        annotations.singleOrNull { it.annotationClass == annotKClass } != null
+
+inline fun <reified T : Annotation> Method.hasAnnot() =
+        hasAnnot(T::class)
