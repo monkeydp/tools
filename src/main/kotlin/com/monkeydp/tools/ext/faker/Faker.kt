765966307ec63a9b9ff2a1e7fc644751e5296674
iPotato-Work <monkeydpotato@gmail.com>
2020-07-16 20:09:11 +0800

    *

--- src/main/kotlin/com/monkeydp/tools/ext/faker/Faker.kt
+++ src/main/kotlin/com/monkeydp/tools/ext/faker/Faker.kt
@@ -1,6 +1,8 @@
 package com.moonlight.iot.business.fake
 
 import com.github.javafaker.DateAndTime
+import com.github.javafaker.Internet
+import com.monkeydp.tools.constant.Symbol.COLON
 import java.util.concurrent.TimeUnit
 
 /**
@@ -8,3 +10,9 @@ import java.util.concurrent.TimeUnit
  * @date 2020/5/14
  */
 fun DateAndTime.random(atMost: Int = 10, timeUnit: TimeUnit = TimeUnit.DAYS) = future(atMost, timeUnit)
+
+/**
+ * Without `:`
+ */
+fun Internet.macAddressSimplify() =
+        macAddress().split(COLON).joinToString("")
