1f667d388611bdaddb40783ae0be91ee048a15b6
iPotato-Work <monkeydpotato@gmail.com>
2020-08-05 14:47:54 +0800

    temp

--- src/main/kotlin/com/monkeydp/tools/task/Task.kt
+++ src/main/kotlin/com/monkeydp/tools/task/Task.kt
@@ -1,12 +1,21 @@
 package com.monkeydp.tools.task
 
-interface Task<C : TaskContent> {
+interface Task<ID : Any, C : TaskContent> {
+    val id: ID
+    val name: String
     val content: C
     val executorName: String
+    val info
+        get() =
+            "Task(id = $id, name = $name)"
 }
 
-interface TaskExecutor<C : TaskContent> {
-    fun execute(task: Task<C>)
+abstract class BaseTask<ID : Any, C : TaskContent> : Task<ID, C>
+
+interface TaskExecutor<T : Task<*, *>> {
+    fun execute(task: T)
 }
+
+abstract class BaseTaskExecutor<T : Task<*, *>> : TaskExecutor<T>
 
 interface TaskContent
