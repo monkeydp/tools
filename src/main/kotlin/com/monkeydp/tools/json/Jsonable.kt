77726f26f55d81a10ebfcfa67be834a7e07dd94d
iPotato-Work <monkeydpotato@gmail.com>
2020-08-10 13:35:43 +0800

    *

---
+++ src/main/kotlin/com/monkeydp/tools/json/Jsonable.kt
@@ -0,0 +1,20 @@
+package com.monkeydp.tools.json
+
+import com.monkeydp.tools.ext.kotlin.toJson
+
+/**
+ * @author iPotato-Work
+ * @date 2020/8/10
+ */
+
+interface Jsonable {
+    fun toJson(): String
+}
+
+abstract class BaseJsonable : Jsonable {
+    override fun toJson() =
+            toJsonX()
+}
+
+private fun Any.toJsonX() =
+        toJson()
