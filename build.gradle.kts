db73ce46cb2ea8c7cc26d0702221ea7ba3537d7a
iPotato-Work <monkeydpotato@gmail.com>
2020-08-31 17:15:19 +0800

    *

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 group = "com.monkeydp"
 version = "1.1.2-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
 
 dependencies {
     // kotlin
