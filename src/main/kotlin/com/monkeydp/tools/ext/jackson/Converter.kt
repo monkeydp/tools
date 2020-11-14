74eb409b4c8a3ef4f5ffd03a61ff64a96fd892e9
iPotato-Work <monkeydpotato@gmail.com>
2020-11-14 18:35:58 +0800

    *

--- src/main/kotlin/com/monkeydp/tools/ext/jackson/Converter.kt
+++ src/main/kotlin/com/monkeydp/tools/ext/jackson/Converter.kt
@@ -1,5 +1,6 @@
 package com.monkeydp.tools.ext.jackson
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.convertValue
 import com.monkeydp.tools.global.objectMapper
 import kotlin.reflect.KClass
@@ -9,30 +10,33 @@ import kotlin.reflect.KClass
  * @date 2020/9/10
  */
 inline fun <reified T> Any.convertValue() =
-        objectMapper.convertValue<T>(this)
+    objectMapper.convertValue<T>(this)
 
 fun <T> Any.convertValue(clazz: Class<T>) =
-        objectMapper.convertValue(this, clazz)
+    objectMapper.convertValue(this, clazz)
 
 fun <T : Any> Any.convertValue(kClass: KClass<T>) =
-        objectMapper.convertValue(this, kClass.java)
+    objectMapper.convertValue(this, kClass.java)
+
+fun <T> Any.convertValue(typeRef: TypeReference<T>) =
+    objectMapper.convertValue(this, typeRef)
 
 inline fun <reified T> Iterable<Any>.convertValue() =
-        map { objectMapper.convertValue<T>(it) }.toList()
+    map { objectMapper.convertValue<T>(it) }.toList()
 
 
 fun Any.toJson() =
-        objectMapper.writeValueAsString(this)
+    objectMapper.writeValueAsString(this)
 
 fun <T> String.toObject(clazz: Class<T>): T =
-        objectMapper.readValue(this, clazz)
+    objectMapper.readValue(this, clazz)
 
 fun <T : Any> String.toObject(kClass: KClass<T>): T =
-        objectMapper.readValue(this, kClass.java)
+    objectMapper.readValue(this, kClass.java)
 
 inline fun <reified T> String.toObject(): T =
-        toObject(T::class.java)
+    toObject(T::class.java)
 
 
 fun String.toJsonNode() =
-        objectMapper.readTree(this)!!
+    objectMapper.readTree(this)!!
