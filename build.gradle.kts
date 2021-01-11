f07479eb1014f68553e2c41c7b78b67304dbeda3
iPotato-Work <monkeydpotato@gmail.com>
2021-01-11 09:03:12 +0800

    *

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,27 +24,37 @@ dependencies {
     implementation("org.apache.commons:commons-exec:1.3")
     api("commons-validator:commons-validator:1.7")
     api("commons-io:commons-io:2.4")
+
     // yaml
     implementation("org.yaml:snakeyaml:1.23")
+
     // json
     api("com.fasterxml.jackson.core:jackson-databind:2.10.4")
     api("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.4")
+
     // log
     implementation("org.slf4j:jul-to-slf4j:1.7.30")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     api("org.apache.logging.log4j:log4j-to-slf4j:2.12.1")
+
     // reflection
     api("org.reflections:reflections:0.9.11")
+
     // di
     api("org.kodein.di:kodein-di-generic-jvm:6.4.1")
+
     // jakarta
     api("jakarta.validation:jakarta.validation-api:2.0.2")
+
     // swagger
     api("io.swagger:swagger-annotations:1.5.22")
+
     //  servlet
     api("javax.servlet:javax.servlet-api:4.0.1")
+
     // faker
     api("com.github.javafaker:javafaker:1.0.2")
+
     // test
     testImplementation(kotlin("test-junit5"))
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0")
