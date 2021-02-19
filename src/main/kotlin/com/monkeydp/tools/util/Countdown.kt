c3c52a8ad49fc60cba5bd1786a1240a7da1762f4
iPotato-Work <monkeydpotato@gmail.com>
2021-02-19 16:43:14 +0800

    *

---
+++ src/main/kotlin/com/monkeydp/tools/util/Countdown.kt
@@ -0,0 +1,35 @@
+package com.monkeydp.tools.util
+
+import com.monkeydp.tools.ext.kotlin.toDays
+import com.monkeydp.tools.ext.kotlin.toSeconds
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
+
+/**
+ * @author iPotato-Work
+ * @date 2021/2/19
+ */
+object Countdown {
+    fun format(time: Long, unit: TimeUnit = MILLISECONDS) =
+            time.toSeconds(unit).let {
+                LeftTime(
+                        days = time.toDays(unit),
+                        hours = it / (60 * 60),
+                        minutes = (it / 60) % 60,
+                        seconds = it % 60,
+                )
+            }
+}
+
+class LeftTime(
+        val days: Long,
+        val hours: Long,
+        val minutes: Long,
+        val seconds: Long,
+) {
+    /**
+     * 格式 - dd HH:mm:ss
+     */
+    override fun toString() =
+            String.format("%d %02d:%02d:%02d", days, hours, minutes, seconds)
+}
